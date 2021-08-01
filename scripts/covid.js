9d71c5fd424746f9e45b4dd5d8907a2ed93764d8
BrenuLevi <brenosoares4303@gmail.com>
2021-08-01 20:17:18 -0300

    update index.html and covid.js

--- scripts/covid.js
+++ scripts/covid.js
@@ -10,7 +10,21 @@ request.responseType = 'json'
 request.send()
 
 request.onload = function () {
-  let test = request.response
+  let { response } = request
 
-  console.log(test.data)
+  console.log(response)
+
+  document.querySelector('.cases p').innerHTML = response.data.cases
+  document.querySelector('.confirmed-cases p').innerHTML =
+    response.data.confirmed
+  document.querySelector('.deaths p').innerHTML = response.data.deaths
+
+  let date = response.data.updated_at.substr(0, 10).split('-')
+  let time = response.data.updated_at.substr(11, 8)
+  console.log(time)
+  document.querySelector('.covid-date-time').innerHTML =
+    'Dados atualizados em: ' +
+    new Date(date[0], date[1] - 1, date[2]).toLocaleDateString() +
+    ' às ' +
+    time
 }
