213078f11eda43bd08d64994852bb8bd2d3b102a
BrenuLevi <brenosoares4303@gmail.com>
2021-08-03 21:06:02 -0300

    added comments for javascript files

--- scripts/covid.js
+++ scripts/covid.js
@@ -1,19 +1,25 @@
+// ? Create HTTP request
 const request = new XMLHttpRequest()
 
+// ? Open HTTP request
 request.open(
   'GET',
   'https://covid19-brazil-api.now.sh/api/report/v1/brazil',
   true
 )
 
+// ? Set type of response
 request.responseType = 'json'
+// ? Send request
 request.send()
 
+// ? When response availabe, get the response
 request.onload = function () {
   let { response } = request
 
   let numberFormatter = new Intl.NumberFormat('pt-br')
 
+  // ? Data
   document.querySelector('.cases p').innerHTML = numberFormatter.format(
     response.data.cases
   )
@@ -23,8 +29,8 @@ request.onload = function () {
     response.data.deaths
   )
 
+  // ? Date of update
   let date = response.data.updated_at.substr(0, 10).split('-')
-  let time = response.data.updated_at.substr(11, 8)
   document.querySelector('.covid-date-time').innerHTML =
     'Dados atualizados em: ' +
     new Date(date[0], date[1] - 1, date[2]).toLocaleDateString()
--- scripts/dayinformation.js
+++ scripts/dayinformation.js
@@ -26,22 +26,25 @@ let months = [
 
 let date = new Date()
 
+//* ======================= Clock ======================== //
 setInterval(() => {
   date = new Date()
   document.querySelector('.clock-text').innerHTML = date.toLocaleTimeString()
 }, 1000)
 
+// ? Timezone
 let dateTimeZone = Date().slice(
   Date().indexOf('(') + 1,
   Date().lastIndexOf(')')
 )
-
 document.querySelector('.fuse-text').innerHTML = dateTimeZone
 
+// ? GMT
 let utcHour = date.getUTCHours()
 document.querySelector('.gmt-text').innerHTML =
   'GMT' + (date.getHours() - utcHour)
 
+//* ======================= Date ======================== //
 let dateDay = date.getDate()
 let dateMonth = date.getMonth() + 1
 let dateYear = date.getFullYear()
@@ -53,11 +56,13 @@ dateText.innerHTML =
   '/' +
   dateYear
 
+// ? Change month's number for month's name when mouse over
 dateText.addEventListener('mouseover', () => {
   dateText.innerHTML =
     returnFormatedData(dateDay) + '/' + months[dateMonth] + '/' + dateYear
 })
 
+// ? Change month's name for month's number when mouse out
 dateText.addEventListener('mouseout', () => {
   dateText.innerHTML =
     returnFormatedData(dateDay) +
@@ -67,9 +72,11 @@ dateText.addEventListener('mouseout', () => {
     dateYear
 })
 
+//?  Week day
 let weekDay = date.getDay()
 document.querySelector('.week-day-text').innerHTML = 'Hoje é: ' + week[weekDay]
 
+// ? Add zero in front of number < 10
 function returnFormatedData(input) {
   if (input < 10) {
     return `0${input}`
